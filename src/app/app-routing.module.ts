7a100eec5d5bc44901a812abeb3a93c764c7c218
Mohitraj Shah <mohitraj.shah@vistex.com>
2022-11-11 13:58:29 +0530

    Child Navigation support

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { MaintainPatientsComponent } from './maintain-patients/maintain-patients.component';
 import { PatientsComponent } from './patients/patients.component';
 import { ChildComponent } from './child/child.component';
+import { MaintainChildComponent } from './maintain-child/maintain-child.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'Patients', component: PatientsComponent},
   { path: 'Patient/:id', component: MaintainPatientsComponent},
-  { path: 'Child',component: ChildComponent}
+  { path: 'Child',component: ChildComponent},
+  { path: 'Child/:id', component: MaintainChildComponent},
 ];
 
 @NgModule({
